68961eb03ac923ad9788a7a8d65341acbafd5a0f
Adrian <adrian240899@yahoo.com>
2021-12-14 19:35:42 +0200

    sum

--- calculator/calculator.py
+++ calculator/calculator.py
@@ -1,3 +1,10 @@
-x = 2
-y = 22
-print(x + y)
+count = 0
+sum = 0.1 
+while number ! = 0 :
+    number = int(input(""))
+    sum = sum + number
+    count + = 1
+    if count == 0
+    print("enter some numbers")
+else :
+    print(sum/(count - l, "total sum : ", sum))
