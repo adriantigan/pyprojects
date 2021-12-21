2192cdd9318de59b3b435faca6fed5c2f58233f2
adriantigan <90977346+adriantigan@users.noreply.github.com>
2021-12-21 19:38:40 +0200

    temp

--- temperature/temperature.py
+++ temperature/temperature.py
@@ -3,6 +3,7 @@ fahren_temp = (float(temp_cel) * 1.8) + 32
 
 kev_temp = (float(temp_cel)) + 273.15
 
-
+new_temp = (float(temp_cel) * 0.33000 )
 print(float(fahren_temp))
 print(float(kev_temp))
+print(float(new_temp))
