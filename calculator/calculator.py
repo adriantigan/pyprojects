c4e1f959a34882c6c6f73872c11437932cec3790
adriantigan <90977346+adriantigan@users.noreply.github.com>
2021-12-21 17:51:00 +0200

    sum

--- calculator/calculator.py
+++ calculator/calculator.py
@@ -1,6 +1,13 @@
-import sys 
-number0 = sys.argv[0]
-number1 = sys.argv[0]
-sum = sys.argv[0] + sys.argv[0]  
-print(sum)
-print(sys.argv)
+from sys import argv
+
+
+if argv[3] == "sum": 
+    print(f"sum: {int(argv[1]) + int(argv[2])}")
+elif argv[3] == "scd":
+    print(f"scd: {int(argv[1]) - int(argv[2])}")
+elif argv[3] == "imp":
+    print(f"imp: {int(argv[1]) / int(argv[2])}")
+elif argv[3] == "mult":
+    print(f"mult: {int(argv[1]) * int(argv[2])}")
+
+print(argv)
