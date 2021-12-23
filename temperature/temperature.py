49f4fade3477123003f8874332fbda1fa2ed91a0
adriantigan <90977346+adriantigan@users.noreply.github.com>
2021-12-23 07:07:41 +0200

    temp

--- temperature/temperature.py
+++ temperature/temperature.py
@@ -1,23 +1,15 @@
-temp_cel = input('Enter temperature: ')
-fahren_temp = (float(temp_cel) * 1.8) + 32
+from sys import argv
 
-kev_temp = (float(temp_cel)) + 273.15
+temp_cel = (float(argv[1]))
 
-new_temp = (float(temp_cel) * 0.33000 )
-print(float(fahren_temp))
-print(float(kev_temp))
-print(float(new_temp))
+fahren_temp = (float(argv[1]) * 1.8) + 32
 
-"""
-1.
-pune sa stiu ce temperatura i fiecare si scoate input in pula mea
+new_temp = (float(argv[1]) * 0.33000 )
+
+
 kev_temp = (float(argv[1])) + 273.15
-print(f"Kelvin temperature is: {kev_temp}")
 
-2.
-dupa ce faci asta extinde programu sa converteasca dintr o temperatura
-in alta
-ex. python temperature.py 23 newton kelvin
-23 de grade newton in kelvin
-etc.
-"""
+print(f"Kelvin temperature is: {kev_temp}")
+print(f"fahrenheit temperature is: {fahren_temp}")
+print(f"newton temperature is:{new_temp}")
+print(f"Celsius temperature is: {temp_cel}")
