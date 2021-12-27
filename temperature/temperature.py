fd1219d9b148925b8b97a63ec02373c22d8a0f87
adriantigan <90977346+adriantigan@users.noreply.github.com>
2021-12-27 19:47:29 +0200

    temp

--- temperature/temperature.py
+++ temperature/temperature.py
@@ -1,15 +1,13 @@
 from sys import argv
 
-temp_cel = (float(argv[1]))
+def K_to_C(temp):
+        return temp + 273.15
 
-fahren_temp = (float(argv[1]) * 1.8) + 32
+def C_to_F(temp): 
+        return (temp * (9/5)) + 32
+K = 286
 
-new_temp = (float(argv[1]) * 0.33000 )
+C = K_to_C(K)
 
+F = C_to_F(C)
 
-kev_temp = (float(argv[1])) + 273.15
-
-print(f"Kelvin temperature is: {kev_temp}")
-print(f"fahrenheit temperature is: {fahren_temp}")
-print(f"newton temperature is:{new_temp}")
-print(f"Celsius temperature is: {temp_cel}")
