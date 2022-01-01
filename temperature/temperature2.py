1c51417bca4648be05fbb81344de1c3d35208cef
adriantigan <90977346+adriantigan@users.noreply.github.com>
2022-01-01 19:22:02 +0200

    cel

---
+++ temperature/temperature2.py
@@ -0,0 +1,10 @@
+from sys import argv
+
+kev_temp = ((float(argv[1]))+ 273.15) 
+print(f"Kelvin temperature is: {kev_temp}")
+
+temp_cel = ((float(argv[1])) * 9 / 5 + 32)
+print(f"Celsius temperature is: {temp_cel}")
+
+
+
