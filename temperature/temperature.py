2155088eebe16dfdf62e19026305926947c90ebb
adriantigan <90977346+adriantigan@users.noreply.github.com>
2022-01-01 19:05:04 +0200

    temp

--- temperature/temperature.py
+++ temperature/temperature.py
@@ -1,11 +1,14 @@
 from sys import argv
 
-if argv[3] == "temp_cel": 
-    print(f""temp_cel: {int("C * 9 / 5 + 32)}"")
-C = int(input("C="))
-elif argv[3] == "feh_temp":
-    print(f""feh_temp: {int("F * 1,8) + 32)}"")
-    F = int(input("F="))
-elif argv[3] == "kev_temp":
-    K = int(input("K="))
-    print(f""kev_temp: {int("K - 273,15)}"")
+if argv[3] == "cel": 
+
+    print("cel: {int(argv[1]) * 9 / 5 + 32)}")
+elif argv[3] == "feh":
+
+    print(f"feh: {{int(argv[1]) * 1,8) + 32)}}")
+
+elif argv[3] == "kev":
+
+    print(f"kev: {{int(argv[1]) - 273,15)}}")
+
+print(argv)
