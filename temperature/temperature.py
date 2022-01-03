6be06f2b11b40efc69e4256e35a54c74953a6c44
adriantigan <90977346+adriantigan@users.noreply.github.com>
2022-01-03 17:24:57 +0200

    temp

--- temperature/temperature.py
+++ temperature/temperature.py
@@ -1,13 +1,13 @@
 from sys import argv
-if argv[3] == "kelvin":
+
 kev_temp = (float(argv[1]))+ 273.15
 print(f"Kelvin temperature is: {kev_temp}")
 
-if argv[3] == "celsius":
+
 temp_cel = (float(argv[1])) * 9 / 5 + 32
 print(f"Celsius temperature is: {temp_cel}")
 
-if argv[3] == "fehrenheit":
+
 feh_temp = (float(argv[1])) - 273,15 
 print(f'Feh temp is: {feh_temp}')
 
