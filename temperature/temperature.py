c6845ab4891897d201b3b0d464998439823f9f9b
Alex Malan <alexmalan@192.168.1.6>
2022-01-25 11:00:32 +0200

    Noi detalii

--- temperature/temperature.py
+++ temperature/temperature.py
@@ -1,5 +1,44 @@
 from sys import argv 
 
+"""
+Extinde functionalitatea programului asa incat
+sa nu mai presupunem ca numaru ce il dai ii in
+grade Celsius, ci tu sa specifici in ce grade e
+--------------------------------------------------------
+Implementarea curenta:
+python temperature.py 23 kelvin
+
+23 - presupunem ca is 23 grade celsius
+kelvin - in ce grade convertim 23(celsius to kelvin)
+--------------------------------------------------------
+Implementare noua:
+python temperature.py 23 celsius kelvin
+
+23 - un numar oarecare
+celsius - presupunem ca numarul oarecare e in celsius
+kelvin - gradele in care sa fie transformate cele 23 grade celsius
+
+Exemple:
+python temperature.py 2312 kelvin celsius
+    2312 grade kelvin in celsius
+python temperature.py 32 fahrenheit kelvin
+    2312 grade fahrenheit in kelvin
+python temperature.py 77 kelvin fahrenheit
+    77 grade kelvin in fahrenheit
+etc.
+
+Acuma ca stii sa folosesti functii, creeaza noi
+functii sau extinde-le pe cele actuale asa incat
+sa calculeze toate combinatiile:
+    kelvin_to_celsius
+    fahrenheit_to_kelvin
+    celsius_to_fahrenheit
+    etc.
+
+La verificarea cu ifurile si elifurile trebuie sa
+verifici ambele argumente(ambele cuvinte date la terminal)
+asa incat sa stii ce functie sa apelezi 
+"""
 
 def kelvin(celsius_grades):
     return float(celsius_grades) + 273.15
