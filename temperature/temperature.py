68a47024a66b18f4fa11cb3a554f1254244890fb
adriantigan <90977346+adriantigan@users.noreply.github.com>
2022-02-17 20:22:28 +0200

    tip

--- temperature/temperature.py
+++ temperature/temperature.py
@@ -1,18 +1,50 @@
-from sys import argv 
+from sys import argv
 
 
-def kelvin(celsius_grades):
-    return float(celsius_grades) + 273.15
-
-def fahrenheit(celsius_grades):
-    return float(celsius_grades) * 9 / 5 - 32
+def temperature(c_to_f):
+    return float(c_to_f) * 9 / 5 + 32
+    
 
 
-if argv[2] == "kelvin": 
-    print(f"Kelvin temperature is: {kelvin(argv[1])}")
+def fahrenheit_to_celsius(temp):
+     return float(temp) * 9 / 5 - 32
 
-elif argv[2] == "celsius": 
-    print(f"Celsius temperature is: {argv[1]}")  
 
-elif argv[2] == "fahrenheit":   
-    print(f'Fahrenheit temperature is: {fahrenheit(argv[1])}')
+def celsius_to_kelvin(temp):
+    return float(temp)  + 237.15
+
+def kelvin_to_celsius(temp):
+      return float(temp) - 273.15
+    
+
+
+def fahrenheit_to_kelvin(temp):
+    return 5 / 9 * (float(temp) - 32 ) + 273.15
+
+
+def kelvin_to_fahrenheit(temp):
+    return 1.8 * (float(temp)- 273.15)  + 32 
+
+
+
+
+# def kelvin(celsius_grades):
+#     return float(celsius_grades) - 273.15
+
+
+# def fahrenheit(celsius_grades):
+#     return float(celsius_grades) * 9 / 5 - 32
+
+
+
+
+    
+# if argv[2] == "kelvin": 
+#     print(f"Kelvin temperature is: {kelvin(argv[1])}")
+
+#     print(f"celsius temperature is: ")
+# if argv[2] == "celsius": 
+#     print(f"Celsius temperature is: {argv[1]}")  
+
+# elif argv[2] == "fahrenheit":   
+#     print(f'Fahrenheit temperature is: {fahrenheit(argv[1])}')
---
+++ tipsCalc.py
@@ -0,0 +1,11 @@
+print("Welcome to the tip calculator")
+
+bill = float(input("What was the total bill? € "))
+tip = int(input("How much tip would you like to give? 5, 10 or 15?"))
+people = int(input("how many people to split the bill? "))
+tips_as_percent = tip / 100 
+total_tip_amount = bill * tips_as_percent
+total_bill = bill + total_tip_amount
+bill_per_person = total_bill / people
+final_amount = round(bill_per_person, 2 )
+print(f"Each person should pay: ${final_amount}")
