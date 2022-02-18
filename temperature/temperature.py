dbe8a1cb96e5909ae9734c93d79f953619f98bef
adriantigan <90977346+adriantigan@users.noreply.github.com>
2022-02-18 22:52:21 +0200

    treasure

--- temperature/temperature.py
+++ temperature/temperature.py
@@ -1,30 +1,45 @@
 from sys import argv
 
 
+celsius_to_fahrenheit = int(float()) * 9 / 5 + 32
+f_to_c = int(float())  * 9 / 5 - 32
+
+c_to_k = float() + 237.15
+
+
+
+if celsius_to_fahrenheit == "celsius_to_fahrenheit": 
+    print(f"Celsius temp in fah is: {celsius_to_fahrenheit}")
+    
+elif celsius_to_fahrenheit != "fahrenheit_to_celsius":
+    print(f"Fah temp in chls is: {f_to_c}")
+
 
 def temperature(c_to_f):
     return float(c_to_f) * 9 / 5 + 32
     
 
 
-def fahrenheit_to_celsius(temp):
-     return float(temp) * 9 / 5 - 32
+    
+
+# def fahrenheit_to_celsius(temp):
+#      return float(temp) * 9 / 5 - 32
 
 
-def celsius_to_kelvin(temp):
-    return float(temp)  + 237.15
+# def celsius_to_kelvin(temp):
+#     return float(temp)  + 237.15
 
-def kelvin_to_celsius(temp):
-      return float(temp) - 273.15
+# def kelvin_to_celsius(temp):
+#       return float(temp) - 273.15
     
 
 
-def fahrenheit_to_kelvin(temp):
-    return 5 / 9 * (float(temp) - 32 ) + 273.15
+# def fahrenheit_to_kelvin(temp):
+#     return 5 / 9 * (float(temp) - 32 ) + 273.15
 
 
-def kelvin_to_fahrenheit(temp):
-    return 1.8 * (float(temp)- 273.15)  + 32 
+# def kelvin_to_fahrenheit(temp):
+#     return 1.8 * (float(temp)- 273.15)  + 32 
 
 
 
